e80e70675d1d6b27b1edd1d6fa246829cf446840
Çetin DOĞU <cetindogu@gmail.com>
2023-12-28 18:14:04 +0300

    fix: only one .UseMessageRetry method must be used.

--- OrderService/Program.cs
+++ OrderService/Program.cs
@@ -9,11 +9,14 @@ var bus = BusConfigurator.ConfigureBus(configuration =>
         // Variations
         // e.Consumer(() => new SubmitOrderCommandConsumer());
         // e.Consumer(typeof(SubmitOrderCommandConsumer), type => Activator.CreateInstance(type));
+
         e.Consumer<SubmitOrderCommandConsumer>();
 
-        e.UseMessageRetry(r => r.Immediate(5));
-
-        e.UseMessageRetry(r => r.Ignore(typeof(ArgumentNullException), typeof(DivideByZeroException)));
+        e.UseMessageRetry(r =>
+        {
+            r.Immediate(5);
+            r.Ignore(typeof(ArgumentNullException), typeof(DivideByZeroException));
+        });
 
         e.UseRateLimit(1000, TimeSpan.FromSeconds(5));
 
